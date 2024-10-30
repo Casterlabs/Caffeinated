b9177b7f298b9b6458421a3de56883d05975b30f
e3ndr <33337309+e3ndr@users.noreply.github.com>
2024-10-30 11:47:19 -0500

    App: Reorganize the files.
    This is for compatibility with GH packages, which the app is going to use for the SDK distribution ;)

--- dev.sh
+++ dev.sh
@@ -5,4 +5,4 @@ cd app
 # Copy the widget loader to the appropriate places...
 ./mvnw generate-sources
 # Then run the dev servers.
-npx concurrently "cd Caffeinated/src/main/ui && npm install && npm run dev" "cd BuiltInPlugins/src/main/widgets && npm install && npm run dev"
+npx concurrently "cd ui && npm install && npm run dev" "cd built_in_plugins/src/main/widgets && npm install && npm run dev"
